b0f22c3959eaafc38ed579445f788458d753f1c9
JdGXII <jralzaibar@yahoo.com>
2023-09-19 13:38:15 -0500

    fixed to MercadoLibrePage and final results.txt file added

--- pages/mercado_libre.py
+++ pages/mercado_libre.py
@@ -13,7 +13,13 @@ class MercadoLibrePage:
         return self.page.query_selector_all("li.ui-search-layout__item")
 
     def extract_product_data(self, product):
-        name = product.text_content("h2.ui-search-item__title")
-        price = product.text_content("span.price-tag-amount")
-        link = product.get_attribute("href", "a.ui-search-item__group__element.ui-search-link")
+        name_element = product.query_selector("h2.ui-search-item__title")
+        name = name_element.text_content() if name_element else None
+    
+        price_element = product.query_selector("span.price-tag-amount")
+        price = price_element.text_content() if price_element else None
+    
+        link_element = product.query_selector("a.ui-search-item__group__element.ui-search-link")
+        link = link_element.get_attribute("href") if link_element else None
+    
         return name, price, link
